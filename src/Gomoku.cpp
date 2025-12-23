d2bc2148641e2e764bc9e2ee14a17e77ee3c9141
kojilbj <20020427ka@gmail.com>
2025-12-23 12:57:02 +0100

    feature: pass ailevel and turnorder to gamescene

--- src/Gomoku.cpp
+++ src/Gomoku.cpp
@@ -9,7 +9,11 @@ Gomoku::Gomoku() : _window(sf::VideoMode({1080, 1000}), "Gomoku"), font() {
   this->_resultScene = std::make_unique<ResultScene>(font, this->_window.getSize());
   this->_currentScene = this->_menuScene.get();
 
-  this->_menuScene->setOnStartGame([this]() { changeScene(this->_gameScene.get()); });
+  this->_menuScene->setOnStartGame([this](TurnOrder turnOrder, AILevel& level) {
+    this->_gameScene.get()->setAILevel(level);
+    this->_gameScene.get()->setTurnOrder(turnOrder);
+    changeScene(this->_gameScene.get());
+  });
   this->_gameScene->setOnGameOver([this](const std::string& winner) {
     this->_window.clear();
     this->_gameScene->render(this->_window);
