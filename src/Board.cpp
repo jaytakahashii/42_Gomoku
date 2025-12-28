79a601df9ec7bdd88f5a25dc762ab04b484ddd6a
Jay Takahashi <qsakura14555@gmail.com>
2025-12-29 00:21:18 +0100

    refa: del debug comment

--- src/Board.cpp
+++ src/Board.cpp
@@ -67,7 +67,6 @@ bool Board::checkWin() {
           if (_isStoneCapturable(stoneIdx, myStones, oppStones)) {
             // 一つでも捕獲される石があれば、このラインでの勝利は成立しない
             lineIsSafe = false;
-            std::cout << "Capturable!!" << std::endl;  // TODO: デバッグ用
             break;
           }
         }
