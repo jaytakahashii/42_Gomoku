4dbc5a87a930c08f3b2c95973cfa782e2bddcb0a
kojilbj <20020427ka@gmail.com>
2026-01-13 14:41:29 +0100

    feature: handle sigint

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,22 @@
 #include <SFML/Graphics.hpp>
 #include <chrono>
 #include <cmath>
+#include <csignal>
 #include <iostream>
 #include <optional>
 #include <string>
 #include <vector>
 
+Gomoku* g_pointer = nullptr;
+
+void signalHandler(int sig) {
+  g_pointer->stop();
+}
+
 int main() {
   Gomoku game;
+  g_pointer = &game;
+  std::signal(SIGINT, signalHandler);
   game.run();
   return 0;
 }
